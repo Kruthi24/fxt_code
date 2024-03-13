4989db2a5e64b26d3c4747a50da1a8acd0ac84e4
kruthi24 <kruthi24p3@gmail.com>
2024-03-14 03:48:29 +0530

    add documentation to lc_lmfit

--- lc_lmfit.py
+++ lc_lmfit.py
@@ -35,14 +35,23 @@ def get_y(res, n, x):
 
 
 def power_law(x,alpha_1,amplitude):
+    '''
+    Calculate the power law function.
+    '''
     return amplitude * x ** (-alpha_1)
 
 def broken_power_law(x,t_break,alpha_1,alpha_2,amplitude):
+    '''
+    Calculate the broken power law function.
+    '''
     alpha = np.where(x< t_break, alpha_1, alpha_2)
     tt = x / t_break
     return amplitude * tt ** (-alpha)
 
 def double_broken_law(x, tb0, tb1, alpha_0, alpha_1, alpha_2, amplitude):
+    '''
+    Calculate the double broken power law function.
+    '''
     x0 = x[np.where(x<tb0)]
     x1 = x[np.where((x>tb0)&(x<tb1))]
     x2 = x[np.where(x>tb1)]
@@ -53,6 +62,9 @@ def double_broken_law(x, tb0, tb1, alpha_0, alpha_1, alpha_2, amplitude):
     return amplitude * x_return
 
 def triple_broken_law(x, tb0, tb1, tb2, alpha_0, alpha_1, alpha_2, alpha_3, amplitude):
+    '''
+    Calculate the triple broken power law function.
+    '''
     x0 = x[np.where(x<tb0)]
     x1 = x[np.where((x>tb0)&(x<tb1))]
     x2 = x[np.where((x>tb1)&(x<tb2))]
@@ -65,18 +77,24 @@ def triple_broken_law(x, tb0, tb1, tb2, alpha_0, alpha_1, alpha_2, alpha_3, ampl
     return amplitude * x_return
 
 def nbroken_law(x, breaks, alphas, amplitude):
-  breaks.append(np.max(x))
-  n_seg = len(alphas)
-  x_seg = x[np.where(x<=breaks[0])]**(-alphas[0])
-  scaling_factors = 1
-  for i in range(1,n_seg):
-    x_chunk = x[np.where((x>=breaks[i-1])&(x<=breaks[i]))]
-    scaling_factors = scaling_factors * breaks[i-1]**(alphas[i]-alphas[i-1])
-    x_seg = np.append(x_seg,scaling_factors * x_chunk**(-alphas[i]))
-  return amplitude * (breaks[0])**(alphas[0]) * x_seg
+    '''
+    Calculate the n-broken power law function.
+    '''
+    breaks.append(np.max(x))
+    n_seg = len(alphas)
+    x_seg = x[np.where(x<=breaks[0])]**(-alphas[0])
+    scaling_factors = 1
+    for i in range(1,n_seg):
+        x_chunk = x[np.where((x>=breaks[i-1])&(x<=breaks[i]))]
+        scaling_factors = scaling_factors * breaks[i-1]**(alphas[i]-alphas[i-1])
+        x_seg = np.append(x_seg,scaling_factors * x_chunk**(-alphas[i]))
+    return amplitude * (breaks[0])**(alphas[0]) * x_seg
 
 
 def lnlikehood(x, y, yerr, theta,BPL):
+    '''
+    Calculate the log-likelihood of the data given the model parameters.
+    '''
     yerr=np.array(yerr)
     if(BPL==True):
         t_break,alpha_1,alpha_2,amplitude = theta
@@ -87,24 +105,36 @@ def lnlikehood(x, y, yerr, theta,BPL):
     return -0.5*(np.sum((y-model)**2/(yerr**2) + np.log(np.sqrt(yerr**2)*2*pi)))
 
 def information_criteria(ln_like,y,theta):
+    '''
+    Calculate the AIC, BIC, and degrees of freedom.
+    '''
     AIC=-2*ln_like+(2*len(theta))+(2*len(theta)*len(theta))
     BIC=-2*ln_like+(len(theta)*np.log(len(y)))
     dof=len(y)-len(theta)
     return AIC,BIC,dof
 
 def lnlike(theta, x, y, yerr):
+    '''
+    Calculate the log-likelihood of the data given the model parameters.
+    '''
     t_break,alpha_1,alpha_2,amplitude = theta
     model = broken_power_law(x,t_break,alpha_1,alpha_2,amplitude)
     inv_sigma2 = 1.0/(yerr**2)
     return -0.5*(np.sum((y-model)**2*inv_sigma2 - np.log(inv_sigma2)))
 
 def lnprior(theta):
+    '''
+    Calculate the log-prior of the model parameters.
+    '''
     t_break,alpha_1,alpha_2,amplitude = theta
     if 1e1<t_break<1e4 and -1.0<alpha_1<0.2 and 0.0<alpha_2<4.0 and 1e-15<amplitude<1e-11:
         return 0.0
     return -inf
 
 def lnprob(theta, x, y, yerr):
+    '''
+    Calculate the log-posterior of the model parameters.
+    '''
     lp = lnprior(theta)
     if not isfinite(lp):
         return -inf
@@ -113,6 +143,9 @@ def lnprob(theta, x, y, yerr):
 
 #objective functions
 def cost_func_pl(params,x,y,x_err,y_err, orth=False):
+    '''
+    Calculate the cost function for the power law model.
+    '''
     v = params.valuesdict()
     y_model = power_law(x,v["alpha_1"],v["amplitude"])
     if orth == True:
@@ -121,6 +154,9 @@ def cost_func_pl(params,x,y,x_err,y_err, orth=False):
     return cost_fn
 
 def cost_func_bpl(params,x,y,x_err,y_err, orth=False):
+    '''
+    Calculate the cost function for the broken power law model.
+    '''
     v = params.valuesdict()
     y_model = broken_power_law(x,v["t_break"],v["alpha_1"],v["alpha_2"],v["amplitude"])
     if orth == True:
@@ -129,6 +165,9 @@ def cost_func_bpl(params,x,y,x_err,y_err, orth=False):
     return cost_fn
 
 def cost_func_dbl(params, x, y, x_err, y_err, orth=False):
+    '''
+    Calculate the cost function for the double broken power law model.
+    '''
     v = params.valuesdict()
     y_model = double_broken_law(x,v["tb0"],v["tb1"],v["alpha_0"],v["alpha_1"],v["alpha_2"],v["amplitude"])
     if orth == True:
@@ -137,6 +176,9 @@ def cost_func_dbl(params, x, y, x_err, y_err, orth=False):
     return cost_fn
 
 def cost_func_nbpl(params, x, y, x_err, y_err, n, orth=False):
+    '''
+    Calculate the cost function for the n-broken power law model.
+    '''
     v = params.valuesdict()
     tbreaks = [v["tb"+str(i)] for i in range(n-1)]
     alphas = [v["alpha_"+str(i)] for i in range(n)]
